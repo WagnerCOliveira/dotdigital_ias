89e9ce9d793260edc01b4858508731754e2c7c1e
wagnercoliveira <wagner.costa.oliveira@gmail.com>
2022-08-06 11:26:59 -0300

    Atualizado script install docker para ubuntu

--- terraform/provisionamento/files/docker-install.sh
+++ terraform/provisionamento/files/docker-install.sh
@@ -10,8 +10,9 @@ echo "#################### Instalando Pre-requisitos ########################"
 sudo apt-get install ca-certificates curl gnupg lsb-release git -y
 
 echo "################  Adicionando Repositorios Dokcer CE ##################"
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "###################### Instalando Docker CE ###########################"
 sudo apt-get update
